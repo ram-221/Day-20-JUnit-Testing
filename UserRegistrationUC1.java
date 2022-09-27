99633cdd99fc34350becfd1f423ee96915e4fcf9
ram-221 <rameshmalli857.com>
2022-09-28 01:19:23 +0530

    Added: Functonality to validate firstnae Using Junit

---
+++ UserRegistrationUC1.java
@@ -0,0 +1,32 @@
+package com.bridglabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationUC1 {
+
+	/*
+	 * method to check first name
+	 */
+	public boolean firstName(String firstName) {
+
+		String regex="^([A-Z]{1}[a-z]{2,}";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher match=pattern.matcher(firstName);
+
+		return match.matches();
+
+	}
+
+	/*
+	 * method to chek last name 
+	 */
+
+	public boolean lastName(String lastName) {
+		String regex = "^([A-Z]{1}[a-z]{2,})";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
+}
+
