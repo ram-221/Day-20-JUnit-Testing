aaf0f66c16a3fba5d13ef7269719525e9b83a902
ram-221 <rameshmalli857.com>
2022-09-30 05:59:59 +0530

    Enter valid email ex:- abc.xyz@bl.co.in

--- UserRegistrationUC3.java
+++ UserRegistrationUC3.java
@@ -3,16 +3,11 @@ package com.bridglabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistrationUC2 {
-
-
-
-	/*
-	 * method to check first name
-	 */
+public class UserRegistrationUC3 {
+	
 	public boolean firstName(String firstName) {
 
-		String regex="^([A-Z]{1}[a-z]{2,}";
+		String regex="^[A-Z]{1}[a-z]{3,6}";
 		Pattern pattern=Pattern.compile(regex);
 		Matcher match=pattern.matcher(firstName);
 
@@ -25,9 +20,20 @@ public class UserRegistrationUC2 {
 	 */
 	
 	public boolean lastName(String lastName) {
-		String regex = "^([A-Z]{1}[a-z]{2,})";
+		String regex = "^[A-Z]{1}[a-z]{3,6}";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(lastName);
 		return matcher.matches();
 	}
+
+	
+	/*
+	 *  Mathod to check emaile
+	 */
+	 public boolean emailAddress(String email) {
+	        String regex = "^[a-z]{3}(.+)[a-z]{3}@[bl](.+)[co](.+)";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(email);
+	        return matcher.matches();
+	    }
 }
