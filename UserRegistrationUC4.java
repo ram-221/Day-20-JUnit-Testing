16e0965cd6b01ffa165a42547962c16cdd294579
ram-221 <rameshmalli857.com>
2022-09-30 06:04:19 +0530

    Country code by space and digit number using Test Cases

--- UserRegistrationUC4.java
+++ UserRegistrationUC4.java
@@ -3,8 +3,12 @@ package com.bridglabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistrationUC3 {
-	
+public class UserRegistrationUC4 {
+
+
+	/*
+	 * method to check first name
+	 */
 	public boolean firstName(String firstName) {
 
 		String regex="^[A-Z]{1}[a-z]{3,6}";
@@ -36,4 +40,17 @@ public class UserRegistrationUC3 {
 	        Matcher matcher = pattern.matcher(email);
 	        return matcher.matches();
 	    }
+
+	 /*
+	   * method to validate mobile number
+	   *
+	   */
+	   public boolean phoneNumber(String phoneNumber) {
+	        String regex = "^(91)?[7-9][0-9]{9}";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(phoneNumber);
+	        return matcher.matches();
+	    }
+
 }
+
